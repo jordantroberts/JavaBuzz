27d236032e7f20f464c7703aaea6b5d3c42b76cb
Jordan <jordanroberts@talk21.com>
2019-04-29 17:12:50 +0100

    Tests and code written, needs refactoring

---
+++ src/fizzbuzz.js
@@ -0,0 +1,13 @@
+var Fizzbuzz = function(){};
+
+  Fizzbuzz.prototype.isDivisibleByThree = function(number) {
+    return (number % 3 === 0);
+  };
+
+  Fizzbuzz.prototype.isDivisibleByFive = function(number) {
+    return (number % 5 === 0);
+  };
+
+  Fizzbuzz.prototype.isDivisibleByFifteen = function(number) {
+    return (number % 15 === 0);
+  };
