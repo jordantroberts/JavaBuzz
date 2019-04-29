33fe45f3b2ac2a8c611d1824c97227ac4ccc3456
Jordan <jordanroberts@talk21.com>
2019-04-29 17:31:51 +0100

    Refactored

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,13 +1,17 @@
 var Fizzbuzz = function(){};
 
+  Fizzbuzz.prototype._isDivisibleBy = function(number, divisor) {
+    return (number % divisor === 0);
+  };
+
   Fizzbuzz.prototype.isDivisibleByThree = function(number) {
-    return (number % 3 === 0);
+    return this._isDivisibleBy(number, 3);
   };
 
   Fizzbuzz.prototype.isDivisibleByFive = function(number) {
-    return (number % 5 === 0);
+    return this._isDivisibleBy(number, 5);
   };
 
   Fizzbuzz.prototype.isDivisibleByFifteen = function(number) {
-    return (number % 15 === 0);
+    return this._isDivisibleBy(number, 15);
   };
